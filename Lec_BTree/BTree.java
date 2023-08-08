24c4d2841e0aa6500277b3de43b85f3f2b9d1ce7
Lakshya Singh <78645656+Lakshya-CB@users.noreply.github.com>
2023-08-08 13:23:26 +0530

    Add files via upload

--- Lec_BTree/BTree.java
+++ Lec_BTree/BTree.java
@@ -1,5 +1,8 @@
 package Lec_BTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BTree {
 	class Node {
 		public Node(int i) {
@@ -29,12 +32,12 @@ public class BTree {
 
 	public BTree(int[] pre, int[] in) {
 		// TODO Auto-generated constructor stub
-		root = createPreIn(pre, 0, pre.length-1, in, 0, in.length-1);
-		
+		root = createPreIn(pre, 0, pre.length - 1, in, 0, in.length - 1);
+
 	}
 
 	private Node createPreIn(int[] pre, int ps, int pe, int[] in, int is, int ie) {
-		if(ps>pe || is>ie) {
+		if (ps > pe || is > ie) {
 			return null;
 		}
 		Node nn = new Node(pre[ps]);
@@ -47,82 +50,155 @@ public class BTree {
 				break;
 			}
 			L_size++;
-			
+
 		}
-		
+
 		nn.left = createPreIn(pre, ps + 1, ps + L_size, in, is, f - 1);
 		nn.right = createPreIn(pre, ps + 1 + L_size, pe, in, f + 1, ie);
 		return nn;
 	}
+
 	public int size() {
 		return size(root);
 	}
+
 	private int size(Node nn) {
-		if(nn==null) {
+		if (nn == null) {
 			return 0;
 		}
 		int L = size(nn.left);
 		int R = size(nn.right);
-		return L+R+1;
+		return L + R + 1;
 	}
+
 	public int Ht() {
 		return Ht(root);
 	}
+
 	int global = 0;
+
 	private int Ht(Node nn) {
-		if(nn==null) {
+		if (nn == null) {
 			return -1;
 		}
 		int L = Ht(nn.left);
 		int R = Ht(nn.right);
-		int self = L+R+2;
-		global= Math.max(global, self);
-		return 1+Math.max(L, R);
+		int self = L + R + 2;
+		global = Math.max(global, self);
+		return 1 + Math.max(L, R);
 	}
+
 	public int Dia() {
 //		return Dia(root);
 		return Dia2(root).Dia;
 	}
 
 	private int Dia(Node nn) {
-		if(nn==null) {
+		if (nn == null) {
 			return 0;
 		}
 		int L = Dia(nn.left);
 		int R = Dia(nn.right);
-		int self = Ht(nn.left) + Ht(nn.right)+2;
-		
-		return Math.max(Math.max(L, R),self);
+		int self = Ht(nn.left) + Ht(nn.right) + 2;
+
+		return Math.max(Math.max(L, R), self);
 	}
-	class pair{
-		int Dia=0;
-		int Ht=-1;
+
+	class pair {
+		int Dia = 0;
+		int Ht = -1;
 	}
+
 	private pair Dia2(Node nn) {
-		if(nn==null) {
+		if (nn == null) {
 			return new pair();
 		}
 		pair L = Dia2(nn.left);
 		pair R = Dia2(nn.right);
-		int self = L.Ht + R.Ht+2;
-		
-		
-		
-		
+		int self = L.Ht + R.Ht + 2;
+
 //		###########################
 		pair ans = new pair();
-		ans.Ht =  Math.max(L.Ht, R.Ht)+1;
+		ans.Ht = Math.max(L.Ht, R.Ht) + 1;
 		ans.Dia = Math.max(Math.max(L.Dia, R.Dia), self);
 		return ans;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
+
+	int p_idx = 0;
+
+	public BTree(int[] pre) {
+		p_idx = 0;
+		root = createPre(pre);
+	}
+
+	private Node createPre(int[] pre) {
+		if (p_idx >= pre.length || pre[p_idx] == -1) {
+			p_idx++;
+			return null;
+		}
+		Node nn = new Node(pre[p_idx]);
+		p_idx++;
+		nn.left = createPre(pre);
+		nn.right = createPre(pre);
+		return nn;
+	}
+
+	public void lvlPrint() {
+		Queue<Node> Q = new LinkedList<>();
+		Q.add(root);
+		int curr_size = 1;
+//		while(!Q.isEmpty()) {
+		while (curr_size > 0) {
+			for (int cnt = 1; cnt <= curr_size; cnt++) {
+				Node nn = Q.poll();
+				System.out.print(nn.data + " ");
+				if (nn.left != null) {
+					Q.add(nn.left);
+				}
+				if (nn.right != null) {
+					Q.add(nn.right);
+				}
+			}
+			curr_size = Q.size();
+			System.out.println();
+		}
+	}
+
+	public BTree(int[] lvl, boolean b) {
+		root = new Node(lvl[0]);
+		Queue<Node> Q = new LinkedList<>();
+		int idx = 1;
+		while (idx < lvl.length && !Q.isEmpty()) {
+			Node nn = Q.poll();
+			if (lvl[idx] != -1) {
+				nn.left = new Node(lvl[idx]);
+				Q.add(nn.left);
+			}
+			idx++;
+			if (lvl[idx] != -1) {
+				nn.right = new Node(lvl[idx]);
+				Q.add(nn.right);
+			}
+			idx++;
+		}
+	}
+	
+	public boolean isBal() {
+		return isBal(root);
+	}
+
+	private boolean isBal(Node nn) {
+		if(nn==null) {
+			return true;
+		}
+		boolean L = isBal(nn.left);
+		boolean R = isBal(nn.right);
+		boolean self = Math.abs(Ht(nn.left)-Ht(nn.right))<=1;
+		return L&&R&& self;
+	}
+	
+	
+	
+	
 }
--- Lec_BTree/Client__BT.java
+++ Lec_BTree/Client__BT.java
@@ -2,11 +2,12 @@ package Lec_BTree;
 
 public class Client__BT {
 	public static void main(String[] args) {
-		int[] pre = {10,20,40,50,30,60};
-		int[] in = {40,20,50,10,30,60};
+		int[] pre = { 10, 20, 40, 50, 30, 60 };
+		int[] in = { 40, 20, 50, 10, 30, 60 };
 		BTree BT = new BTree(pre, in);
 //		BT.print();
 		System.out.println(BT.size());
 		System.out.println(BT.Dia());
+		BT.lvlPrint();
 	}
 }
